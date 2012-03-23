670b8fc053cc836f68475bc8e795a7d715d660a5
Wong Yong Jie <yjwong92@gmail.com>
2012-03-24 03:35:33 +0800

    Added new makefile that includes all prebuilts for CUPS.
    This should make adding CUPS to a device configuration much easier.
    Also added several missing prebuilts.

---
+++ cups.mk
@@ -0,0 +1,6 @@
+# Include all the Makefiles for the prebuilts.
+include external/cups/data/cups-data.mk
+include external/cups/fonts/cups-fonts.mk
+include external/cups/test/cups-test.mk
+include external/cups/templates/cups-templates.mk
+
